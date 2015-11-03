a88f83f60055c9ce382c9493e0733175477379e9
Chris Montrois <cmontrois@slack-corp.com>
2015-11-03 12:41:31 -0700

    Test json response

--- src/controllers/poopin.js
+++ src/controllers/poopin.js
@@ -1,4 +1,8 @@
 module.exports = function(request, reply) {
   var user = request.payload.user_name;
-  reply(user + ' is poopin');
+  reply({
+    response_type: 'in_channel',
+    text: user + ' is poopin',
+    attachments: [],
+  });
 };
