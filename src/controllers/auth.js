cfc088b8f7895d8afd66ef72fe33cbb296fd011e
Chris Montrois <cmontrois@slack-corp.com>
2015-11-03 13:19:28 -0700

    Fix var shadowing

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,9 +3,9 @@ var request = require('request');
 var CLIENT_ID = process.env.POOPIN_CLIENT_ID;
 var CLIENT_SECRET = process.env.POOPIN_CLIENT_SECRET;
 
-module.exports = function(request, reply) {
-  var code = request.params.code;
-  var state = request.params.state;
+module.exports = function(req, reply) {
+  var code = req.params.code;
+  var state = req.params.state;
 
   request.get('https://slack.com/api/oauth.access', {
     client_id: CLIENT_ID,
