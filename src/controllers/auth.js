9cc090f14fae9f6e1ab3b4f85302789fc1cf879c
Chris Montrois <cmontrois@slack-corp.com>
2015-11-03 13:53:24 -0700

    Param

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,9 +2,11 @@ var request = require('request');
 var qs = require('qs');
 
 module.exports = function(req, res) {
-  var code = req.param('code');
+  var code = req.params.code;
   var client_id = process.env.POOPIN_CLIENT_ID;
   var client_secret = process.env.POOPIN_CLIENT_SECRET;
+
+  console.log('CODE:', code);
 
   var url = 'https://slack.com/api/oauth.access?' + qs.stringify({
     client_id: client_id,
@@ -13,6 +15,6 @@ module.exports = function(req, res) {
   });
   request.post(url, function(err, http, body) {
     console.log('OAUTH.ACCESS:', body);
-    res(true);
+    res.send(true);
   });
 };
