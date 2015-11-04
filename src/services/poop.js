33e9e46d69380b777518afd545a0f2f986aea67b
Chris Montrois <cmontrois@slack-corp.com>
2015-11-03 18:16:35 -0700

    Remove logs and update mongo

--- src/services/poop.js
+++ src/services/poop.js
@@ -10,26 +10,22 @@ var daySchema = new mongoose.Schema({
   users: 'array',
   date: 'string'
 });
-var Day = mongoose.model('Date', daySchema);
+var Day = mongoose.model('Day', daySchema);
 
 module.exports = {
   poopin: function(username) {
     var key = dateKey(moment().toDate());
-
-    console.log('KEY', key);
 
     return new Promise(function(resolve, reject) {
       Day.findOne({ date: key }, function(err, day) {
         if (err) return reject(err);
 
         if (!day) {
-          console.log('NO DAY');
           var user = { name: username, count: 1 };
           return Day.create({
             users: [user],
             date: key
           }, function(err, saved) {
-            console.log('SAVED', err, saved);
             if (err) return reject(err);
             resolve(user);
           });
@@ -37,14 +33,12 @@ module.exports = {
 
         var user = _.find(day.users, { name: username });
         if (!user) {
-          console.log('NO DAY');
           user = { name: username, count: 0 };
           day.users.push(user);
         }
         user.count += 1;
 
         Day.findOneAndUpdate({ date: key }, day, function(err, updated) {
-          console.log('UPDATED', err, updated);
           if (err) return reject(err);
           resolve(user);
         });
@@ -54,11 +48,8 @@ module.exports = {
   report: function(date) {
     var key = dateKey(date);
 
-    console.log('KEY', key);
-
     return new Promise(function(resolve, reject) {
       Day.findOne({ date: key }, function(err, day) {
-        console.log('REPORT', err, day);
         if (err) return reject(err);
         if (!day) return resolve([]);
         resolve(day.users);
